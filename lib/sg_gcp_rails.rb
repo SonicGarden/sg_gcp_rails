715fbea98023be1496ffde0da4d6e7e696cad5a5
ruzia <ruzia@idealab.jp>
2024-05-17 23:18:31 +0900

    CloudSqlActivatorをrequire_relativeに追加

--- lib/sg_gcp_rails.rb
+++ lib/sg_gcp_rails.rb
@@ -2,6 +2,7 @@
 
 require_relative "sg_gcp_rails/version"
 require_relative "sg_gcp_rails/one_off_executor"
+require_relative "sg_gcp_rails/cloud_sql_activator"
 
 module SgGcpRails
   class Error < StandardError; end
