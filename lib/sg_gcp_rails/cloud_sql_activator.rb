b12642043e667e5ce1a3fb688ae637ac1dc8e6f0
ruzia <ruzia@idealab.jp>
2024-05-21 16:09:19 +0900

    cloud-sql-proxyはspawnで起動させるように（実験

--- lib/sg_gcp_rails/cloud_sql_activator.rb
+++ lib/sg_gcp_rails/cloud_sql_activator.rb
@@ -6,9 +6,9 @@ class CloudSqlActivator
   def run
     puts "Activate cloud sql... connection_name: #{@connection_name}"
 
-    `curl -o cloud-sql-proxy https://storage.googleapis.com/cloud-sql-connectors/cloud-sql-proxy/v2.11.1/cloud-sql-proxy.linux.amd64`
+    `curl -s -o cloud-sql-proxy https://storage.googleapis.com/cloud-sql-connectors/cloud-sql-proxy/v2.11.1/cloud-sql-proxy.linux.amd64`
     `chmod +x cloud-sql-proxy`
     `mkdir /cloudsql; chmod 777 /cloudsql`
-    `./cloud-sql-proxy --unix-socket /cloudsql #{@connection_name} &`
+    spawn("./cloud-sql-proxy --unix-socket /cloudsql #{@connection_name}")
   end
 end
